ee210772e9aa418c012f0ee60d15666d310d343e
alekos0610 <sandres.silval@gmail.com>
2020-05-23 18:03:09 -0500

    se cambia nombre de sintaxis2 por primera funcion

---
+++ python/curso/primera función.py
@@ -0,0 +1,14 @@
+# Crear una función, declararla y llamarla
+# Se usa para reutilizar código a futuro.
+
+def mensaje():  # Esta primera linea es llamada declaración de la función
+
+    print("estamos aprendiendo python")
+    print("estamos aprendiendo instrucciones básicas")              # Estas tres lineas son el cuerpo de la función
+    print("estamos aprendiendo poco a poco iremos avanzando")
+
+mensaje()  # Llamar a la función para que pueda imprimirse.
+
+
+
+
