b1be5ba437a48e478fc672b405d161c4eef60a39
alekos0610 <sandres.silval@gmail.com>
2020-05-23 17:20:05 -0500

    se cambia nombre archivo clase u y archivo curso

---
+++ python/curso/sintaxis basica 1.py
@@ -0,0 +1,15 @@
+# definir las dos variables que voy a utilizar
+numero1 = 5
+numero2 = 7
+
+# condicional if (si) para comparar 2 o más opciones y ver si estás se cumplen.
+# Importante recordar la terminación en : luego de las variables.
+# Importante: el texto indexado para evitar errores de sintaxis 
+
+if numero1>numero2:
+    print("El número 1 es mayor")
+else:
+    print("el número 2 es mayor")
+
+# Este código arroja como resultado (El numero 2 es mayor)
+
