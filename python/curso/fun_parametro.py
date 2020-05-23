55529c05d749d99c08595efa86160e3afb04ea01
alekos0610 <sandres.silval@gmail.com>
2020-05-23 18:28:07 -0500

    Se crea el archivo fun_parametro.py, video 6 curso python

---
+++ python/curso/fun_parametro.py
@@ -0,0 +1,11 @@
+# Creación de una función con parámetros
+
+def suma(num1, num2):       # Creación de la función
+
+    resultado = num1 + num2 # Creación de una variable
+
+    return resultado        # solicitar retorno de la variable
+
+almacena_resultado = suma(5,8)
+
+print (almacena_resultado)
