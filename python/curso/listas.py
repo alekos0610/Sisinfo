0b54dea1c4eaa21cd9a5cf57aace839d03ebec67
alekos0610 <sandres.silval@gmail.com>
2020-05-23 19:47:17 -0500

    se crea el archivo listas.py y se realiza ejercicio listas, video 7

---
+++ python/curso/listas.py
@@ -0,0 +1,23 @@
+# Creación de una lista = nombre + dentro de corchetes los datos
+
+milista = ["Maria", "5", "True", "78.35"]
+
+# milista = ["Maria", "5", "True", "78.35"] * 3;  el operador multiplicar(*), imprime la lista la cantidad de veces solicitada 
+
+# append("sandra"); Inserta un elemento en la lista en la última posición
+
+# insert(2,"Sandra"); inserta un elemento a la lista en la posición seleccionada
+
+# milista.extend(["Sandra", "Ana", "Lucia"])   Extend permite agregar varios elementos a la lista
+
+# print(milista.index("Ana"))  Index devuelve el indice (posición) del elemento solicitado
+
+# print("Pepe" in milista) in, imprime true o false, dependiendo si el elemento consultado está en la lista
+
+# milista.pop("Ana") Remove, elimina el objeto seleccionado de la lista
+
+# milista.pop(), Elimina el último elemento de la lista
+
+# milista3 = milista + milista2   # El operador suma funciona como concatenador de dos listas diferentes.
+
+print(milista[:])
