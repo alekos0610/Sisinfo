21bb1f1ce3bab19a3475fd4768a0d2d819356ebf
alekos0610 <sandres.silval@gmail.com>
2020-05-24 00:08:37 -0500

    Se crea el archivo tuplas.py y se edita segun video 8

---
+++ python/curso/tuplas.py
@@ -0,0 +1,28 @@
+# Ejercicio de creación de tuplas
+
+# se puede crear sin parentesis, pero no se recomienda para evitar errores
+
+
+
+mitupla = ("Juan", 13, 1, 1995) # a diferencia de la lista va encerrada en parentesis
+nombre, dia, mes, agno = mitupla #desempaquetado de tupla.
+
+print(nombre)
+print(agno)
+print(mes)
+print(dia)
+
+
+
+
+
+# milista = list(mitupla) # convertir una tupla en una lista
+
+
+# Proceso donde se convirtió una lista en una tupla
+
+""" milista = ["juan", 13, 1, 13, 1995]
+
+mitupla = tuple(milista)
+
+print(len(mitupla))"""
